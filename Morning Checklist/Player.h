ede2dc0c0d4e6c53efc3eea18279a6e706979214
J.J.Gallucci <jacobgalluch@gmail.com>
2025-02-06 14:36:38 -0500

    Same

--- Morning Checklist/Player.h
+++ Morning Checklist/Player.h
@@ -7,47 +7,49 @@
 #include "tile.h"
 /* Need to merge instead of two seperate classes have grapplehook be apart of Player, polymorphism? */
 
-class Player {
+class Player
+{
 public:
-	sf::RectangleShape p_box_;
-	float x{40.f};
-	float y{250.f};
+    sf::RectangleShape p_box_;
+    float x{40.f};
+    float y{450.f};
     float multiplier{40.f};
-	sf::Vector2f velocity{50.f, 0.f};
+    sf::Vector2f velocity{50.f, 0.f};
     float gravity = 15.f;
     float friction = 15.f;
-	float jump_velocity = 15.f;
-    int player_speed = 40;
+    float jump_velocity{-25.f};
+    float player_speed{40.f};
     float delta_time;
-	bool colliding{false};
-	sf::Vector2f player_pos{x, y};
-	sf::Vector2f player_size{30.f, 30.f};
+    bool colliding{false};
+    sf::Vector2f player_pos{x, y};
+    sf::Vector2f player_size{30.f, 30.f};
 
-	Player();
+    Player();
 
-	void update(float);
-	void collision(tile& object);
+    void update(float);
+    void collision(tile& object);
 };
 
-class GrappleHook {
+class GrappleHook
+{
 public:
-	sf::Vector2f mouse_pos;
-	//Grapple Hook and Grapple Line
-	sf::RectangleShape g_line;
-	float hook_x{10.f};
-	float hook_y{5.f};
-	bool hook_active{false};
-	bool hook_hit{false};
-	float rotation{0.f};
-	float grap_length{1.f};
+    sf::Vector2f mouse_pos;
+    //Grapple Hook and Grapple Line
+    sf::RectangleShape g_line;
+    float hook_x{10.f};
+    float hook_y{5.f};
+    bool hook_active{false};
+    bool hook_hit{false};
+    float rotation{0.f};
+    float grap_length{1.f};
     float dt_ = 0;
-    
+
     GrappleHook() = default;
     GrappleHook(Player& player);
-	void Update(Player&, sf::Vector2f, tile, float);
+    void Update(Player&, sf::Vector2f, tile, float);
     static void Collision(Player& player);
-	float GrappleRotation(sf::Vector2f m_pos, sf::Vector2f p_pos);
-	float GrappleLength(sf::Vector2f m_pos, sf::Vector2f p_pos);
-	sf::Vector2f GrappleSlope(sf::Vector2f m_pos, sf::Vector2f p_pos);
-	void RetractGrapple();
+    float GrappleRotation(sf::Vector2f m_pos, sf::Vector2f p_pos);
+    float GrappleLength(sf::Vector2f m_pos, sf::Vector2f p_pos);
+    sf::Vector2f GrappleSlope(sf::Vector2f m_pos, sf::Vector2f p_pos);
+    void RetractGrapple();
 };
